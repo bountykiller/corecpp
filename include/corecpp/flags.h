4a1489f2b7d0b4592635722f372d726f28ec46ac
bountykiller <masse.nicolas@gmail.com>
2019-03-22 23:14:16 +0100

    * Make some adjustement and add some methods in the flags API

--- include/corecpp/flags.h
+++ include/corecpp/flags.h
@@ -20,21 +20,21 @@ public:
 	{}
 	explicit constexpr flags(underlying_type e = 0) : m_value(e)
 	{}
-	constexpr flags operator +(EnumT e) const { return flags( m_value + static_cast<underlying_type>(e)); } /* or should it be m_value | static_cast<underlying_type>(e));  ?*/
-	constexpr flags operator -(EnumT e) const { return flags( m_value - static_cast<underlying_type>(e)); } /* or should it be m_value & ~static_cast<underlying_type>(e)); ?*/
+	constexpr flags operator +(EnumT e) const { return flags( m_value | static_cast<underlying_type>(e)); }
+	constexpr flags operator -(EnumT e) const { return flags( m_value & ~static_cast<underlying_type>(e)); }
 	constexpr flags operator |(EnumT e) const { return flags( m_value | static_cast<underlying_type>(e)); }
 	constexpr flags operator &(EnumT e) const { return flags( m_value & static_cast<underlying_type>(e)); }
 	constexpr flags operator ^(EnumT e) const { return flags( m_value ^ static_cast<underlying_type>(e)); }
 
-	constexpr flags operator +(flags f) const { return flags(m_value + f.m_value); }
-	constexpr flags operator -(flags f) const { return flags(m_value - f.m_value); }
+	constexpr flags operator +(flags f) const { return flags(m_value | f.m_value); }
+	constexpr flags operator -(flags f) const { return flags(m_value & ~f.m_value); }
 	constexpr flags operator |(flags f) const { return flags(m_value | f.m_value); }
 	constexpr flags operator &(flags f) const { return flags(m_value & f.m_value); }
 	constexpr flags operator ^(flags f) const { return flags(m_value ^ f.m_value); }
 
 	flags& operator =(EnumT e) { m_value = static_cast<underlying_type>(e); return *this; }
-	flags& operator +=(EnumT e) { m_value += static_cast<underlying_type>(e); return *this; }
-	flags& operator -=(EnumT e) { m_value -= static_cast<underlying_type>(e); return *this; }
+	flags& operator +=(EnumT e) { m_value |= static_cast<underlying_type>(e); return *this; }
+	flags& operator -=(EnumT e) { m_value &= ~static_cast<underlying_type>(e); return *this; }
 	flags& operator |=(EnumT e) { m_value |= static_cast<underlying_type>(e); return *this; }
 	flags& operator &=(EnumT e) { m_value &= static_cast<underlying_type>(e); return *this; }
 	flags& operator ^=(EnumT e) { m_value ^= static_cast<underlying_type>(e); return *this; }
@@ -49,10 +49,13 @@ public:
 	flags& operator =(underlying_type value) { m_value = value; return *this; }
 
 	constexpr flags operator ~() const { return ~m_value; }
-
-	constexpr underlying_type get() const
+	constexpr bool operator ==(flags f) const
 	{
-		return m_value;
+		return m_value == f.m_value;
+	}
+	constexpr bool operator ==(EnumT e) const
+	{
+		return m_value == static_cast<underlying_type>(e);
 	}
 	constexpr bool operator !() const
 	{
@@ -65,6 +68,50 @@ public:
 	explicit constexpr operator underlying_type() const
 	{
 		return m_value;
+	}
+
+	constexpr underlying_type get() const
+	{
+		return m_value;
+	}
+	flags& set(EnumT e)
+	{
+		m_value |= static_cast<underlying_type>(e);
+		return *this;
+	}
+	template <typename... Args>
+	flags& set(EnumT e, Args&&... values)
+	{
+		m_value |= static_cast<underlying_type>(e);
+		set(std::forward<Args>(values)...);
+		return *this;
+	}
+	flags& unset(EnumT e)
+	{
+		m_value &= ~static_cast<underlying_type>(e);
+		return *this;
+	}
+	template <typename... Args>
+	flags& unset(EnumT e, Args&&... values)
+	{
+		m_value &= ~static_cast<underlying_type>(e);
+		unset(std::forward<Args>(values)...);
+		return *this;
+	}
+
+	constexpr bool is_set(EnumT e) const
+	{
+		return ((m_value & e) == e);
+	}
+	template <typename... Args>
+	constexpr bool is_all_set(EnumT e, Args&&... values) const
+	{
+		return is_set(e) && is_all_set(std::forward<Args>(values)...);
+	}
+	template <typename... Args>
+	constexpr bool is_one_set(EnumT e, Args&&... values) const
+	{
+		return is_set(e) || is_one_set(std::forward<Args>(values)...);
 	}
 };
 
